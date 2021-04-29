f21214efd97f72493455505fbe5c8f6f9138804e
Koushik PLN <koushikpln02@gmail.com>
2021-04-29 09:50:28 +0530

    donr

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 app.get("/", function(req, res){
     res.send("Basic site");
 })
-
+app.get("/home", function(req, res){
+    res.send("Welcome home!!");
+})
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
 
