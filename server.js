9510a2dbc2f44edfaa6f5e9f761f675a4233142e
Koushik PLN <koushikpln02@gmail.com>
2021-04-30 10:16:09 +0530

    site

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.get("/home", function(req, res){
 app.get("/res", function(req, res){
     res.sendFile(__dirname+"/Resume-Koushik-PLN.pdf");
 })
-
+app.get("/site", function(req, res){
+    res.sendFile(__dirname+"/frontend/html/site.html");
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
